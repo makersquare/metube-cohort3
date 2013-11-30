001bd3d51690bfbef66860138954b89be57f9ced
MakerSquare Student <students@themakersquare.com>
2013-11-30 16:00:30 -0600

    adding shows controller and view pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ MetubeCohort3::Application.routes.draw do
   get '/movies/gladiator' => 'movies#show_gladiator'
   get '/movies/matilda' => 'movies#show_matilda'
   
+  get '/shows/seinfeld' => 'shows#show_seinfeld'  
+  get '/shows/friends' => 'shows#show_friends'  
 end
