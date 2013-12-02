d2f79f7f4fcc75f8060248c886b4140c4c9e9547
MakerSquare Student <students@themakersquare.com>
2013-12-02 12:34:06 -0600

    finishing the afternoon part of the lesson

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,36 +1,71 @@
 class VideosController < ApplicationController
   
-  def show_gladiator
-    @movie = {
-      title: 'Gladiator',
-      video_id: 'FI1ylg4GKv8',
-      description: "Crowe shows everyone what's up."
-    }
-  end
-
-  def show_matilda
-    @movie = {
-      title: 'Matilda',
-      video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
-    }
+  def show_all
     
   end
 
-  def show_seinfeld
-    @movie = {
-      title: 'Seinfeld',
-      video_id: 'm5OoR0Qjrlo',
-      description: 'A great description on where socks really go'
+  def show
+    # Below is some code that should help you get started.
+    # Feel free to write the code any which way you please.
+    video_name = params["video_name"]
+    videos = {
+      "gladiator" => {
+        title: 'Gladiator',
+        video_id: 'FI1ylg4GKv8',
+        description: "Crowe shows everyone what's up."
+      },
+      "matilda" => {
+        title: 'Matilda',
+        video_id: 'EVWOQwZENBg',
+        description: "Quite the random movie, but a classic for sure!"
+      },
+      "seinfeld" => {
+        title: 'Seinfeld',
+        video_id: 'm5OoR0Qjrlo',
+        description: 'A great description on where socks really go'
+      },
+      "friends" => {
+        title: 'Friends',
+        video_id: 'XIsVHF2bwVs',
+        description: 'Gotta love friends!'
+      }
     }
+
+    @video = videos[video_name]
   end
 
-  def show_friends
-    @movie = {
-      title: 'Friends',
-      video_id: 'XIsVHF2bwVs',
-      description: 'Gotta love friends!'
+  def show_two
+    videos = {
+      "gladiator" => {
+        title: 'Gladiator',
+        video_id: 'FI1ylg4GKv8',
+        description: "Crowe shows everyone what's up."
+      },
+      "matilda" => {
+        title: 'Matilda',
+        video_id: 'EVWOQwZENBg',
+        description: "Quite the random movie, but a classic for sure!"
+      },
+      "seinfeld" => {
+        title: 'Seinfeld',
+        video_id: 'm5OoR0Qjrlo',
+        description: 'A great description on where socks really go'
+      },
+      "friends" => {
+        title: 'Friends',
+        video_id: 'XIsVHF2bwVs',
+        description: 'Gotta love friends!'
+      }
     }
+
+    video_one_name = params["insert here"]
+    video_two_name = params["insert here"]
+
+
+    @video_one = "insert here"
+    @video_two = "insert here"
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 MetubeCohort3::Application.routes.draw do
 
-  get '/gladiator' => 'videos#show_gladiator'
-  get '/matilda' => 'videos#show_matilda'  
-  get '/seinfeld' => 'videos#show_seinfeld'  
-  get '/friends' => 'videos#show_friends'
+  root 'videos#show_all'
+
+  get '/videos' => 'videos#show_all', as: 'videos'
+  get '/videos/:video_name' => 'videos#show', as: 'video'
 
 end
