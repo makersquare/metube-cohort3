623a04dd21646468c0d630ae61ef4cc7b433d173
MakerSquare Student <students@themakersquare.com>
2013-12-02 23:16:30 -0600

    adding class variable for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,35 @@
 class VideosController < ApplicationController
 
   def show
-    @video = 
+    video_name = params[:video_name_from_user]
+    @video = @@videos[video_name]
   end
   
   def show_all
-    
+    @videos = @@videos
   end
+
+  @@videos = {
+    "gladiator" => {
+      title: 'Gladiator',
+      video_id: 'FI1ylg4GKv8',
+      description: "Crowe shows everyone what's up."
+    },
+    "matilda" => {
+      title: 'Matilda',
+      video_id: 'EVWOQwZENBg',
+      description: "Quite the random movie, but a classic for sure!"
+    },
+    "seinfeld" => {
+      title: 'Seinfeld',
+      video_id: 'm5OoR0Qjrlo',
+      description: 'A great description on where socks really go'
+    },
+    "friends" => {
+      title: 'Friends',
+      video_id: 'XIsVHF2bwVs',
+      description: 'Gotta love friends!'
+    }
+  }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MetubeCohort3::Application.routes.draw do
 
-  get '/videos' => 'videos#show'
-  
+  root 'videos#show_all'
+  get '/videos' => 'videos#show_all'
+  get '/videos/:video_name_from_user' => 'videos#show', as: 'video'
   
 end
