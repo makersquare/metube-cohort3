315535911858e91df78a2bb73eec14287f8b29a2
MakerSquare Student <students@themakersquare.com>
2013-12-02 22:52:42 -0600

    removing hardcoded data to prep for activerecord lesson

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,36 +1,11 @@
 class VideosController < ApplicationController
+
+  def show
+
+  end
   
-  def show_gladiator
-    @movie = {
-      title: 'Gladiator',
-      video_id: 'FI1ylg4GKv8',
-      description: "Crowe shows everyone what's up."
-    }
-  end
-
-  def show_matilda
-    @movie = {
-      title: 'Matilda',
-      video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
-    }
+  def show_all
     
-  end
-
-  def show_seinfeld
-    @movie = {
-      title: 'Seinfeld',
-      video_id: 'm5OoR0Qjrlo',
-      description: 'A great description on where socks really go'
-    }
-  end
-
-  def show_friends
-    @movie = {
-      title: 'Friends',
-      video_id: 'XIsVHF2bwVs',
-      description: 'Gotta love friends!'
-    }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 MetubeCohort3::Application.routes.draw do
 
-  get '/videos/gladiator' => 'videos#show_gladiator'
-  get '/videos/matilda' => 'videos#show_matilda'  
-  get '/videos/seinfeld' => 'videos#show_seinfeld'  
-  get '/videos/friends' => 'videos#show_friends'
-
+  get '/videos' => 'videos#show'
+  
+  
 end
