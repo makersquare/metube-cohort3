7f7197b54be131f826e44074da902b69f04d2f3a
MakerSquare Student <students@themakersquare.com>
2013-12-03 01:13:16 -0600

    adding seed data for the videos table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Video.destroy_all
+
+videos = {
+  "gladiator" => {
+    title: 'Gladiator',
+    youtube_id: 'FI1ylg4GKv8',
+    description: "Crowe shows everyone what's up."
+  },
+  "matilda" => {
+    title: 'Matilda',
+    youtube_id: 'EVWOQwZENBg',
+    description: "Quite the random movie, but a classic for sure!"
+  },
+  "seinfeld" => {
+    title: 'Seinfeld',
+    youtube_id: 'm5OoR0Qjrlo',
+    description: 'A great description on where socks really go'
+  },
+  "friends" => {
+    title: 'Friends',
+    youtube_id: 'XIsVHF2bwVs',
+    description: 'Gotta love friends!'
+  }
+}
+
+videos.each do |key, video|
+  Video.create(video)
+end
