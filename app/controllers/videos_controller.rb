16c83b4585552e5495ed0cd7f834d993e7a81612
MakerSquare Student <students@themakersquare.com>
2013-12-03 00:10:47 -0600

    changign to youtube_id

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,22 +12,22 @@ class VideosController < ApplicationController
   @@videos = {
     "gladiator" => {
       title: 'Gladiator',
-      video_id: 'FI1ylg4GKv8',
+      youtube_id: 'FI1ylg4GKv8',
       description: "Crowe shows everyone what's up."
     },
     "matilda" => {
       title: 'Matilda',
-      video_id: 'EVWOQwZENBg',
+      youtube_id: 'EVWOQwZENBg',
       description: "Quite the random movie, but a classic for sure!"
     },
     "seinfeld" => {
       title: 'Seinfeld',
-      video_id: 'm5OoR0Qjrlo',
+      youtube_id: 'm5OoR0Qjrlo',
       description: 'A great description on where socks really go'
     },
     "friends" => {
       title: 'Friends',
-      video_id: 'XIsVHF2bwVs',
+      youtube_id: 'XIsVHF2bwVs',
       description: 'Gotta love friends!'
     }
   }
