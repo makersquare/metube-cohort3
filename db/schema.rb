baf258879b73049244dea33effc14ba7c40987ca
Shaan Shah <shahshaan@gmail.com>
2013-12-05 23:34:20 -0600

    adding foundation to stylesheets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205052925) do
+ActiveRecord::Schema.define(version: 20131205193924) do
 
   create_table "ratings", force: true do |t|
     t.integer  "value"
@@ -24,12 +24,10 @@ ActiveRecord::Schema.define(version: 20131205052925) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email_address", null: false
+    t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email_address"], name: "index_users_on_email_address"
 
   create_table "videos", force: true do |t|
     t.string   "title"
