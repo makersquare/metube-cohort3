e288c59e004aca49be4774315a53c73d290c225d
Shaan Shah <shahshaan@gmail.com>
2013-12-06 03:27:54 -0600

    adding model call back fro youtube api call

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source 'https://rubygems.org'
 
 gem 'hirb'
 
+gem 'unirest'
+
+gem 'figaro'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,12 @@
 class Video < ActiveRecord::Base
+
   belongs_to :user
   has_many :ratings
 
+  after_create :youtube_query
+
   validates :user_id, :youtube_id, :title, presence: true
+
   def avg_rating
     total = 0.0
     self.ratings.to_a.each do |rating|
@@ -13,5 +17,11 @@ class Video < ActiveRecord::Base
     else
       return total / self.ratings.count
     end
+  end
+
+  def youtube_query
+    response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=020sZ0PY8ko&key=#{ENV['YOUTUBE_API_TOKEN']}")
+    self.description = response.body["items"][0]['snippet']['description']
+    self.save
   end
 end
